97b0eeb61e5cb56206042b7fa0628b36b7f3b1ec
Lawrenceliwanag <112452327+Lawrenceliwanag@users.noreply.github.com>
2022-09-07 16:14:27 +0800

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Age:");
+        int age = scanner.nextInt();
+        
+        System.out.println("GPA: ");
+        double GPA = scanner.nextDouble();
+        
+        System.out.println("Bloodtype: ");
+        scanner.nextLine();
+        String bloodType = scanner.nextLine();
+        
+        System.out.println("Hi, My name is "+name);
+        System.out.println("I am "+age+" Years old");
+        System.out.println("My GPA is "+GPA);
+        System.out.println("My Blood type is "+bloodType);
+        
+    }
+}
